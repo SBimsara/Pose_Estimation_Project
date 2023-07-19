d6afcf06c19b32b0881cfe78bbd50afc40d7bf0c
Mad Man <113254019+SBimsara@users.noreply.github.com>
2023-07-19 16:05:15 +0530

    completed landmark detection

--- PoseEstimationMin.py
+++ PoseEstimationMin.py
@@ -7,17 +7,21 @@ mpDraw = mp.solutions.drawing_utils
 mpPose = mp.solutions.pose
 pose = mpPose.Pose()
 
-cap = cv2.VideoCapture("Pose Videos/pose_video_3.mp4")
+cap = cv2.VideoCapture("Pose Videos/pose_video_2.mp4")
 pTime = 0
 cTime = 0
 while True:
     success, img = cap.read()
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = pose.process(imgRGB)
-    print(results.pose_landmarks)
+    # print(results.pose_landmarks)
     if results.pose_landmarks:
         mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-
+        for id,lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            print(id,lm)
+            cx, cy = int(lm.x*w), int(lm.y*h)
+            cv2.circle(img,(cx,cy), 10, (255,0,0), cv2.FILLED)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
